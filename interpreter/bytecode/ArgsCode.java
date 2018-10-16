7a0fe2f1de1cb00a7dd7f6b8ad6a4e26297a67e1
Kilan <krai@mail.sfsu.edu>
2018-10-15 20:20:10 -0700

    four commit

--- interpreter/bytecode/ArgsCode.java
+++ interpreter/bytecode/ArgsCode.java
@@ -1,7 +1,7 @@
 package interpreter.bytecode;
 import interpreter.VirtualMachine;
 
-public class ArgsCode extends ByteCode  {
+public class ArgsCode extends ByteCode  {//just for commit
     private int n;
     public void init(String[] args) {
         n = Integer.parseInt(args[1]);
